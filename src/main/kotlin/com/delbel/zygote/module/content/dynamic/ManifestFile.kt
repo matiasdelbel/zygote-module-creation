b1a84d919dc9f25be718eb66fa7eee73f09fe25f
Matias Del Bel <matiasdelbel@gmail.com>
2020-04-30 21:48:33 -0300

    write manifest with xml writer

--- src/main/kotlin/com/delbel/zygote/module/content/dynamic/ManifestFile.kt
+++ src/main/kotlin/com/delbel/zygote/module/content/dynamic/ManifestFile.kt
@@ -1,6 +1,9 @@
 package com.delbel.zygote.module.content.dynamic
 
 import com.delbel.zygote.module.Module
+import java.io.StringWriter
+import javax.xml.stream.XMLOutputFactory
+import javax.xml.stream.XMLStreamWriter
 
 class ManifestFile : DynamicContent() {
 
@@ -10,6 +13,19 @@ class ManifestFile : DynamicContent() {
 
     override fun content(module: Module) = module.contentFor(file = this)
 
-    fun content(packageName: String) =
-        "<manifest xmlns:android=\"http://schemas.android.com/apk/res/android\" package=\"$packageName\"/>"
+    fun content(packageName: String): String {
+        val stringWriter = StringWriter()
+        val xmlWriter: XMLStreamWriter = XMLOutputFactory.newInstance().createXMLStreamWriter(stringWriter)
+
+        xmlWriter.writeStartDocument("UTF-8", "1.0")
+        xmlWriter.writeStartElement("manifest")
+        xmlWriter.writeAttribute("xmlns:android", "http://schemas.android.com/apk/res/android")
+        xmlWriter.writeAttribute("package", packageName)
+        xmlWriter.writeEndElement()
+        xmlWriter.writeEndDocument()
+        xmlWriter.flush()
+        stringWriter.flush()
+
+        return stringWriter.buffer.toString()
+    }
 }
---
+++ src/test/kotlin/com/delbel/zygote/module/content/dynamic/ManifestFileTest.kt
@@ -0,0 +1,19 @@
+package com.delbel.zygote.module.content.dynamic
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class ManifestFileTest {
+
+    @Test
+    fun `content create an android manifest`() {
+        val manifest = ManifestFile()
+
+        val manifestAsString = manifest.content(packageName = "com.test")
+
+        assertEquals(expected(), manifestAsString)
+    }
+
+    private fun expected() =
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><manifest xmlns:android=\"http://schemas.android.com/apk/res/android\" package=\"com.test\"></manifest>"
+}
